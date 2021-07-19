6bed888b4b2d2c8095043ec7ae8db4b6130d0665
LeMasters <jacksonlemasters@outlook.com>
2021-07-19 11:54:28 -0600

    changed some formatting

--- Develop/server.js
+++ Develop/server.js
@@ -3,7 +3,7 @@ const express = require("express");
 const apiRoutes = require("./routes/apiRoutes");
 const htmlRoutes = require("./routes/htmlRoutes");
 
-// Initialize the app and create a port
+// Init app and create port
 const app = express();
 const PORT = process.env.PORT || 8080;
 
@@ -14,5 +14,5 @@ app.use(express.static("public"));
 app.use("/api", apiRoutes);
 app.use("/", htmlRoutes);
 
-// Start the server on the port
+// Start server
 app.listen(PORT, () => console.log(`Listening on PORT: ${PORT}`));
