0c30f572ec075ad7b622e923a7f1bdeb7b8bdd8a
LeMasters <jacksonlemasters@outlook.com>
2021-07-19 12:15:01 -0600

    changed some formatting

--- Develop/db/store.js
+++ Develop/db/store.js
@@ -20,7 +20,7 @@ getNotes() {
     return this.read().then(notes => {
     let parsedNotes;
 
-      // If notes isn't an array or can't be turned into one, send back a new empty array
+      // If notes isn't an array or can't be turned into one, return new empty array
     try {
         parsedNotes = [].concat(JSON.parse(notes));
     } catch (err) {
@@ -38,10 +38,9 @@ addNote(note) {
         throw new Error("Note 'title' and 'text' cannot be blank");
     }
 
-    // Add a unique id to the note using uuid package
+    // Add a unique id to the note using uuid 
     const newNote = { title, text, id: uuidv1() };
 
-    // Get all notes, add the new note, write all the updated notes, return the newNote
     return this.getNotes()
         .then(notes => [...notes, newNote])
         .then(updatedNotes => this.write(updatedNotes))
@@ -49,7 +48,7 @@ addNote(note) {
 }
 
 removeNote(id) {
-    // Get all notes, remove the note with the given id, write the filtered notes
+    // Get all notes, remove the note, and write the filtered notes
     return this.getNotes()
         .then(notes => notes.filter(note => note.id !== id))
         .then(filteredNotes => this.write(filteredNotes));
